4d41ed07bc14b86a851fbc5bd2d24304e6071eee
adejokeade <ondinebae@gmail.com>
2022-09-12 11:51:03 -0700

    5-print_numbers

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-
 /**
- * main - Prints alphabets
- * Description - In upper and lowercase
+ * main - Prints alphabets in lowercase, and then in uppercase
+ *
+ * followed by a new line
+ *
  * Retun: Always 0 (Success)
+ *
  */
 
 int main(void)
